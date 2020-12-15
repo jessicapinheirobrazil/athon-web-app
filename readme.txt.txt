9d23325cd5f2366ca591539055d7c6ec9efefd67
Jessica Brazil <jessicapinheirobrazil@hotmail.com>
2020-12-14 23:11:23 -0300

    first comit

---
+++ readme.txt.txt
@@ -0,0 +1,5 @@
+Algumas observações sobre o projeto:
+
+- Quando baixei a fonte utilizada no título, Cinzel Decorative, percebi algumas peculiaridades que me fizeram 
+questionar se era a mesma utilizada no projeto. Como o Adobe XD informa que é, preferi manter
+de início, mas percebi que era muito diferente. Se tivesse acesso à equipe de UX/UI questionaria sobre isso antes de decidir.
---
+++ scripts/script.js
@@ -0,0 +1,40 @@
+console.log('teste');
+
+// import DateRangePicker from './node_modules/vanillajs-datepicker/js/DateRangePicker.js';
+
+// const elem = document.getElementById('daterangepicker');
+// const rangepicker = new DateRangePicker(elem, {
+//   // ...options
+// }); 
+
+/* pop up */
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+})
+
+/* json */
+$(document).ready(function() {
+    getCrimes();
+});
+
+function getCrimes() {
+    $.ajax({
+        type: "get",
+        cache: false,
+        url: "https://athon-tech-ps.herokuapp.com/crime_types",
+        dataType: "text",
+        error: function (request, error) {
+            console.log(arguments);
+            alert(" Can't do because: " + error);
+        },
+        success: function (request) {
+            var crimeTypes = JSON.parse(request);
+            $(crimeTypes.data.crime_types).each(function (index) {
+                var txType = crimeTypes.data.crime_types[index].tx_type;
+                var crimeId = crimeTypes.data.crime_types[index].id_crime_type;
+                $('#typeofcrime').append($("<option />").val(crimeId).text(txType));
+            });
+        }   
+    });
+}
